94a0022a171d0e28ca284c6b4b45f90977da7fb1
aocattleya <aocattleya@yahoo.co.jp>
2018-10-13 20:24:47 +0900

    ステージクリアの演出を追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,11 +13,18 @@ public class GameManager : MonoBehaviour {
 
     public GameObject goButton;     // ボタン：ゲーム開始
     public GameObject retryButton;  // ボタン：リトライ
+    public GameObject clearText;    // テキスト：クリア
+
+    public AudioClip clearSE;       // 効果音：クリア
+    public AudioSource audioSource; // オーディオソース
 
 	// Use this for initialization
 	void Start () {
         retryButton.SetActive(false);   // リトライボタンを非表示
         isBallMoving = false;           // ボールは「移動中ではない」
+
+        // オーディオソースを取得
+        audioSource = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -53,6 +60,8 @@ public class GameManager : MonoBehaviour {
     // ステージクリア処理
     public void StageClear()
     {
-
+        audioSource.PlayOneShot(clearSE);   // クリア音再生
+        clearText.SetActive(true);          // クリア表示
+        retryButton.SetActive(false);       // リトライボタン非表示
     }
 }
